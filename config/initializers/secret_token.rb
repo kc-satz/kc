0cdc668b49653b7fac393862df5984f2c0835128
Satheesh Kumar KC <kc.satz@gmail.com>
2012-04-26 15:31:40 -0500

    KC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kc::Application.config.secret_token = '287329bb056e3dc6ea8710e8ac1aa288fc630134ef83a0d4a49fcb9c1b8b50939bd0e9efa5a5e77d73003eece168d16838f001c5bb7df61de3b9cde0b8e2007d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kc::Application.config.session_store :cookie_store, key: '_kc_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kc::Application.config.session_store :active_record_store
